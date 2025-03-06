8ba1e65f848ee7f6863e9d50cb7d99c55c87d4f3
rikuyu <rikuyu5884@gmail.com>
2025-03-06 19:10:17 +0900

    connect database

--- app.ts
+++ app.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import {Request, Response} from "express";
+import {connectToDatabase} from "./infra/database/db";
 
 const app = express();
 const port = 8080;
 
-app.listen(port, () => console.log(`✅ Server listening on port: 🚀${port}`));
-
 app.get("/ping", (_: Request, res: Response) => {
     res.send("ok\n");
 });
+
+async function start() {
+    try {
+        await connectToDatabase();
+        app.listen(port, () => console.log(`✅ Server listening on port: 🚀${port}`));
+    } catch (err) {
+        console.log(`❌ error: ${err}`);
+    }
+}
+
+start().catch(console.error);
---
+++ infra/database/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export async function connectToDatabase() {
+    try {
+        await mongoose.connect(process.env.DB_URL as string);
+        console.log("✅ Mongo DB connected successfully");
+    } catch (err) {
+        throw err;
+    }
+}
