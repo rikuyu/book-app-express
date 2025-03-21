b8cf3106ec414f28062e20fe13a276ea74005671
rikuyu <rikuyu5884@gmail.com>
2025-03-22 02:01:29 +0900

    add eslint rule

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,7 +1,13 @@
 import eslint from '@eslint/js';
 import tsEslint from 'typescript-eslint';
+import * as stylistic from "@stylistic/eslint-plugin";
 
 export default tsEslint.config(
     eslint.configs.recommended,
     tsEslint.configs.recommended,
+    stylistic.configs.customize({
+        rules: {
+            "object-curly-spacing": ["error", "always"],
+        },
+    })
 );
